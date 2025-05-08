26a966c17235f966c0d8f9db184058ecc89390e8
Ivan Rodriguez <ivan.rodriguez@utec.edu.pe>
2025-05-08 03:15:36 -0500

    Update main.py

--- microservicio4/app/main.py
+++ microservicio4/app/main.py
@@ -1,5 +1,12 @@
 from fastapi import FastAPI
 from app.api.routes import router
 
-app = FastAPI()
+app = FastAPI(
+    title="Microservicio 4",
+    description="Documentación Swagger personalizada",
+    version="1.0.0",
+    docs_url="/swagger",
+    redoc_url=None
+)
+
 app.include_router(router)
