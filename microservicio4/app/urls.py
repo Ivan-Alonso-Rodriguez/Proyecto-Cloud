696add6bdfc5dde8549a30499aefb0a8af26fa6e
Ivan Rodriguez <ivan.rodriguez@utec.edu.pe>
2025-05-08 02:56:27 -0500

    Create urls.py

---
+++ microservicio4/app/urls.py
@@ -0,0 +1,26 @@
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework import permissions, routers
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+from orquestador.views import TuVistaViewSet  # importa tus vistas reales
+
+router = routers.DefaultRouter()
+router.register(r'tuvista', TuVistaViewSet)  # registra tus vistas reales
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="API Orquestador",
+      default_version='v1',
+      description="Documentación Swagger del Microservicio 4",
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
+urlpatterns = [
+   path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+   path('admin/', admin.site.urls),
+   path('api/', include(router.urls)),
+]
