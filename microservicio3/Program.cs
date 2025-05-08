ace775dd61b5e3f999fe00379d2e3c1a8564865c
Ivan Rodriguez <ivan.rodriguez@utec.edu.pe>
2025-05-08 01:56:48 -0500

    Update Program.cs

--- microservicio3/Program.cs
+++ microservicio3/Program.cs
@@ -6,7 +6,21 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddSingleton<ImageStorageService>();
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "VetImagesService",
+        Version = "v1"
+    });
+
+    // 👇 ESTA LÍNEA ES LA CLAVE PARA EL ERROR DE IFormFile
+    c.MapType<IFormFile>(() => new Microsoft.OpenApi.Models.OpenApiSchema
+    {
+        Type = "string",
+        Format = "binary"
+    });
+});
 
 
 var app = builder.Build();
