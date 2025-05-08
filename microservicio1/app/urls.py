493c308bdbdb06355349a4c247113a5fea5b3f09
Ivan Rodriguez <ivan.rodriguez@utec.edu.pe>
2025-05-08 02:11:07 -0500

    Update urls.py

--- microservicio1/app/urls.py
+++ microservicio1/app/urls.py
@@ -1,13 +1,18 @@
-from django.contrib import admin
-from django.urls import path, include
-from rest_framework import routers
-from pacientes.views import PropietarioViewSet, MascotaViewSet
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
-router = routers.DefaultRouter()
-router.register(r'propietarios', PropietarioViewSet)
-router.register(r'mascotas', MascotaViewSet)
+schema_view = get_schema_view(
+   openapi.Info(
+      title="API Pacientes",
+      default_version='v1',
+      description="Documentación de Swagger",
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include(router.urls)),
+   path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+   ...
 ]
