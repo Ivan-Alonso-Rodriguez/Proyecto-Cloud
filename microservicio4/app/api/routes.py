c188cd03f52a9e349a8b839f7c4015bca373ae73
Ivan Rodriguez <ivan.rodriguez@utec.edu.pe>
2025-05-08 03:19:59 -0500

    Update routes.py

--- microservicio4/app/api/routes.py
+++ microservicio4/app/api/routes.py
@@ -3,7 +3,7 @@ from app.models.historia import armar_historia_clinica
 
 router = APIRouter()
 
-@router.get("/historia/{consulta_id}")
+@router.get("/historia/{consulta_id}", tags=["Historia Clínica"])
 def obtener_historia_clinica(consulta_id: int):
     historia = armar_historia_clinica(consulta_id)
     if "error" in historia:
