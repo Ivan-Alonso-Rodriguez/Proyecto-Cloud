21fbcc4b61a871a2957fac2454cbe2561d40746e
Ivan Rodriguez <ivan.rodriguez@utec.edu.pe>
2025-05-09 18:05:51 -0500

    Update main.py

--- microservicio4/app/main.py
+++ microservicio4/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.responses import PlainTextResponse
 from app.api.routes import router
 
 app = FastAPI(
@@ -9,4 +10,9 @@ app = FastAPI(
     redoc_url=None
 )
 
+# Agrega para health chec
+@app.get("/", response_class=PlainTextResponse)
+async def root():
+    return "OK"
+    
 app.include_router(router)
