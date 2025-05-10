9502c8e7502749589f6a8582bf069db8ca4be959
Ivan Rodriguez <ivan.rodriguez@utec.edu.pe>
2025-05-09 20:14:56 -0500

    Update main.ts

--- microservicio2/src/main.ts
+++ microservicio2/src/main.ts
@@ -5,11 +5,14 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors();
+
   const config = new DocumentBuilder()
     .setTitle('Consultas API')
     .setDescription('API para consultas médicas veterinarias')
     .setVersion('1.0')
     .build();
+
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('swagger', app, document);
 
