4acedf4d2aeaa9f1374df744554436b2674ea542
Ivan Rodriguez <ivan.rodriguez@utec.edu.pe>
2025-05-10 01:08:34 -0500

    Update app.module.ts

--- microservicio2/src/app.module.ts
+++ microservicio2/src/app.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Consulta } from './entities/consulta.entity';
 import { Tratamiento } from './entities/tratamiento.entity';
@@ -11,44 +10,17 @@ import { HealthController } from './health.controller';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
-
-    TypeOrmModule.forRootAsync({
-      inject: [ConfigService],
-      useFactory: async (config: ConfigService) => {
-        const port = config.get<string>('DB_PORT');
-        const host = config.get('DB_HOST');
-        const username = config.get('DB_USERNAME');
-        const password = config.get('DB_PASSWORD');
-        const database = config.get('DB_NAME');
-
-        console.log('[MS2] Configuración de la BD:', {
-          host,
-          port,
-          username,
-          password,
-          database,
-        });
-
-        if (!port) {
-          throw new Error('[MS2] ❌ La variable DB_PORT no se ha cargado correctamente desde .env');
-        }
-
-        return {
-          type: 'mysql',
-          host: host || 'localhost',
-          port: parseInt(port),
-          username: username || 'root',
-          password: password || 'root',
-          database: database || 'consultasdb',
-          entities: [Consulta, Tratamiento],
-          synchronize: true,
-          retryAttempts: 10,
-          retryDelay: 3000,
-        };
-      },
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: '172.31.21.115',     // IP privada de tu EC2 con MySQL
+      port: 3307,                // Puerto mapeado (3307 externo → 3306 interno)
+      username: 'root',
+      password: 'utec',
+      database: 'consultasdb',
+      entities: [Consulta, Tratamiento],
+      synchronize: true,
+      retryAttempts: 10,
+      retryDelay: 3000,
     }),
 
     TypeOrmModule.forFeature([Consulta, Tratamiento]),
