1896d5c2c4f387a214bb89173c2a35f75e04777e
Ivan Rodriguez <ivan.rodriguez@utec.edu.pe>
2025-05-09 20:20:54 -0500

    Update main.py

--- microservicio4/app/main.py
+++ microservicio4/app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.responses import PlainTextResponse
+from fastapi.middleware.cors import CORSMiddleware 
 from app.api.routes import router
 
 app = FastAPI(
@@ -10,9 +11,16 @@ app = FastAPI(
     redoc_url=None
 )
 
-# Agrega para health chec
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.get("/", response_class=PlainTextResponse)
 async def root():
     return "OK"
-    
+
 app.include_router(router)
